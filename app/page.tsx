61d38c8894b893c0c36e0a07e3cb7770628415c4
Ishan Raj <ishanraj1908@gmail.com>
2025-04-13 00:43:55 +0530

    Fix grid class for responsive layout in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 bg-black">
-      <div className="grid grid-cols-2 m-20">
+      <div className="grid md:grid-cols-2 gird-cols-1 m-20">
         <div className="">
           <h2 className="bg-clip-text text-transparent text-center bg-gray-300 from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-1 md:py-5 relative z-20 font-bold tracking-tight">
             CareerHive
